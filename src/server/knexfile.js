51cd21a98f4c74c9187dc55698bf5c32b9a5b200
myshuker <myshuker@gmail.com>
2020-06-27 17:47:12 +0200

    knexfile updated

--- src/server/knexfile.js
+++ src/server/knexfile.js
@@ -4,13 +4,14 @@ require('dotenv').config({ path: '../../.env' });
 module.exports = {
   development: {
     client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    connection: process.env.DATABASE_URL,
+    //   connection: {
+    //   host: process.env.DB_HOST,
+    //   port: process.env.DB_PORT,
+    //   user: process.env.DB_USER,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    // },
     pool: { min: 0, max: 7 },
     seeds: {
       directory: path.join(__dirname, '/seeds/development'),
@@ -18,13 +19,14 @@ module.exports = {
   },
   production: {
     client: 'mysql2',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    connection: process.env.DATABASE_URL,
+       // connection: {
+    //   host: process.env.DB_HOST,
+    //   port: process.env.DB_PORT,
+    //   user: process.env.DB_USER,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_NAME,
+    // },
     pool: { min: 0, max: 7 },
     seeds: {
       directory: path.join(__dirname, '/seeds/production'),
